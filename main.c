7d86d0e27f98df448a82b7e8c26d485140c6639d
sepal22 <syjang1505@gmail.com>
2017-11-01 17:45:30 -0700

    1st version

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "testlib.h"
+
+int main()
+{
+	printf("add(1,2) = %d\n", add(1,2));
+	printf("sub(1,2) = %d\n", sub(1,2));
+	printf("mul(1,2) = %d\n", mul(1,2));
+	printf("div(1,2) = %d\n", div(1,2));
+}
